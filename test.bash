a59abc943f6e58fa3798ca8678f9b3a4e89dcac3
aoi daiguji <s21c1080uu@s.chibakoudai.jp>
2022-10-27 13:14:14 +0900

    program no seiri & change test.bash

--- test.bash
+++ test.bash
@@ -2,5 +2,16 @@
 # SPDX-FileCopyrightText: 2022 Daiguji Aoi
 # SPDX-License-Identifier; BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
 
-seq 5 | ./plus
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
